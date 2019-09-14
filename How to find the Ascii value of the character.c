b9163ddd728872410edffcbc6dc6d27dba0fb74e
Tariq Javed <55314564+TariqJaved-Cs@users.noreply.github.com>
2019-09-14 23:10:10 +0500

    Add files via upload

---
+++ How to find the Ascii value of the character.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter the character you want to print its ascii value");
+    scanf("%c",&c);
+    printf("The Ascii value of the character %c is %d",c,c);
+    return 0;
+}
